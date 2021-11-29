7a7175c49d1df3668fe0ede2b7c2087eb227791c
EikichiMizuta <akrys12152@gmail.com>
2021-11-29 19:40:24 +0900

    確認

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">home投稿画面</router-link> |
+      <router-link to="/about">about閲覧画面</router-link>
     </div>
     <router-view />
   </div>
