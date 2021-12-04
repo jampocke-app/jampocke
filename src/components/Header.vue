92899674ed412d3e552342769eaa8198c92f497c
EikichiMizuta <akrys12152@gmail.com>
2021-12-05 00:11:10 +0900

    日本語にしたよ

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,77 +32,77 @@
           <option selected value="宮城県">宮城県</option>
           <option selected value="秋田県">秋田県</option>
           <option selected value="山形県">山形県</option>
-          <option selected value="Hukusima">福島県</option>
+          <option selected value="福島県">福島県</option>
         </select>
       </div>
       <div class="kanto">
         <select v-model="selected" v-on:change="gotopage">
           <option value="" disabled selected>関東地方</option>
-          <option selected value="Ibaraki">茨城県</option>
-          <option selected value="Totigi">栃木県</option>
-          <option selected value="Gunma">群馬県</option>
-          <option selected value="Saitama">埼玉県</option>
-          <option selected value="Tiba">千葉県</option>
-          <option selected value="Tokyo">東京都</option>
-          <option selected value="Kanagawa">神奈川県</option>
+          <option selected value="茨城県">茨城県</option>
+          <option selected value="栃木県">栃木県</option>
+          <option selected value="群馬県">群馬県</option>
+          <option selected value="埼玉県">埼玉県</option>
+          <option selected value="千葉県">千葉県</option>
+          <option selected value="東京都">東京都</option>
+          <option selected value="神奈川県">神奈川県</option>
         </select>
       </div>
       <div class="tyubu">
         <select v-model="selected" v-on:change="gotopage">
           <option value="" disabled selected>中部地方</option>
-          <option selected value="Niigata">新潟県</option>
-          <option selected value="Toyama">富山県</option>
-          <option selected value="Isikawa">石川県</option>
-          <option selected value="Hukui">福井県</option>
-          <option selected value="Yamanasi">山梨県</option>
-          <option selected value="Nagano">長野県</option>
-          <option selected value="Gihu">岐阜県</option>
-          <option selected value="Shizuoka">静岡県</option>
-          <option selected value="Aiti">愛知県</option>
+          <option selected value="新潟県">新潟県</option>
+          <option selected value="富山県">富山県</option>
+          <option selected value="石川県">石川県</option>
+          <option selected value="福井県">福井県</option>
+          <option selected value="山梨県">山梨県</option>
+          <option selected value="長野県">長野県</option>
+          <option selected value="岐阜県">岐阜県</option>
+          <option selected value="静岡県">静岡県</option>
+          <option selected value="愛知県">愛知県</option>
         </select>
       </div>
       <div class="kinki">
         <select v-model="selected" v-on:change="gotopage">
           <option value="" disabled selected>近畿地方</option>
-          <option selected value="Mie">三重県</option>
-          <option selected value="Siga">滋賀県</option>
-          <option selected value="Kyoto">京都府</option>
-          <option selected value="Osaka">大阪府</option>
-          <option selected value="Hyogo">兵庫県</option>
-          <option selected value="Nara">奈良県</option>
-          <option selected value="Wakayama">和歌山県</option>
+          <option selected value="三重県">三重県</option>
+          <option selected value="滋賀県">滋賀県</option>
+          <option selected value="京都府">京都府</option>
+          <option selected value="大阪府">大阪府</option>
+          <option selected value="兵庫県">兵庫県</option>
+          <option selected value="奈良県">奈良県</option>
+          <option selected value="和歌山県">和歌山県</option>
         </select>
       </div>
       <div class="tyugoku">
         <select v-model="selected" v-on:change="gotopage">
           <option value="" disabled selected>中国地方</option>
-          <option selected value="Tottori">鳥取県</option>
-          <option selected value="Simane">島根県</option>
-          <option selected value="Hirosima">広島県</option>
-          <option selected value="Okayama">岡山県</option>
-          <option selected value="Yamaguti">山口県</option>
+          <option selected value="鳥取県">鳥取県</option>
+          <option selected value="島根県">島根県</option>
+          <option selected value="広島県">広島県</option>
+          <option selected value="岡山県">岡山県</option>
+          <option selected value="山口県">山口県</option>
         </select>
       </div>
       <div class="sikoku">
         <select v-model="selected" v-on:change="gotopage">
           <option value="" disabled selected>四国地方</option>
-          <option selected value="Tokusima">徳島県</option>
-          <option selected value="Kagawa">香川県</option>
-          <option selected value="Ehime">愛媛県</option>
-          <option selected value="Kouti">高知県</option>
+          <option selected value="徳島県">徳島県</option>
+          <option selected value="香川県">香川県</option>
+          <option selected value="愛媛県">愛媛県</option>
+          <option selected value="高知県">高知県</option>
         </select>
       </div>
       <div class="kyusyu">
         <select v-model="selected" v-on:change="gotopage">
           <option value="" disabled selected>九州地方</option>
-          <option selected value="Hukuoka">福岡県</option>
-          <option selected value="Saga">佐賀県</option>
-          <option selected value="Nagasaki">長崎県</option>
-          <option selected value="Kumamoto">熊本県</option>
-          <option selected value="Ooita">大分県</option>
-          <option selected value="Miyazaki">宮崎県</option>
-          <option selected value="Kagosima">鹿児島県</option>
-          <option selected value="Okinawa">沖縄県</option>
+          <option selected value="福岡県">福岡県</option>
+          <option selected value="佐賀県">佐賀県</option>
+          <option selected value="長崎県">長崎県</option>
+          <option selected value="熊本県">熊本県</option>
+          <option selected value="大分県">大分県</option>
+          <option selected value="宮崎県">宮崎県</option>
+          <option selected value="鹿児島県">鹿児島県</option>
+          <option selected value="沖縄県">沖縄県</option>
         </select>
       </div>
     </header>
