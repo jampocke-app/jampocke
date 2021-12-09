b53a3fa6c8541d9f8be77a7eb0f130313437ab82
oikawakei-web <keitin10009@gmail.com>
2021-12-09 23:37:03 +0900

    loginview整えました

--- src/views/Loginview.vue
+++ src/views/Loginview.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="login">
-    <button @click="googleLogin" class="googleLogin">
-      Googleアカウントでログイン
-    </button>
-    <button @click="go_to_Japan" class="gotojapan">
-      ようこそ！方言の世界へ！
-    </button>
-    <!-- <button @click="logout" class="logout_button">ログアウト</button> -->
+  <div class="view">
+    <div class="hougentabi">日本列島方言旅</div>
+    <p class="dekiru">★このアプリでできること★</p>
+    <p>地域のリアルな話を聞ける！</p>
+    <p>地域の魅力を気軽に発信できる！</p>
+    <p>地域ごとにコミュニティーを形成できる！</p>
+    <p class="tobitati">気になったらログインして方言の世界へ飛び立とう！</p>
+    <div id="login">
+      <button @click="googleLogin" class="googleLogin">
+        Googleアカウントでログイン
+      </button>
+      <button @click="go_to_Japan" class="gotojapan">
+        ようこそ！方言の世界へ！
+      </button>
+    </div>
   </div>
 </template>
 
@@ -32,19 +39,37 @@ export default {
 </script>
 
 <style scoped>
-.logout_button {
-  border: 6px inset #faa9a6;
-  background: rgb(253, 192, 205);
+.view {
+  background-image: url("../assets/travel.jpg");
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
-.logout_button:hover {
-  color: #fff;
-  background: #f50000;
+.hougentabi {
+  font-size: 600%;
+  font-weight: bold;
+  padding-top: 100px;
 }
 
+p {
+  font-size: 200%;
+}
+
+.dekiru {
+  color: rgb(247, 103, 252);
+}
+
+.tobitati {
+  color: rgb(27, 79, 250);
+}
 .googleLogin {
   border: 6px inset #dafafc;
   background: rgb(192, 239, 253);
+  width: 16%;
+  padding: 10px;
+  margin-right: 2%;
+  font-weight: bold;
 }
 
 .googleLogin:hover {
@@ -55,6 +80,11 @@ export default {
 .gotojapan {
   border: 6px inset #d0face;
   background: rgb(206, 253, 192);
+  width: 16%;
+  padding: 10px;
+  margin-left: 2%;
+  margin-top: 2%;
+  font-weight: bold;
 }
 
 .gotojapan:hover {
